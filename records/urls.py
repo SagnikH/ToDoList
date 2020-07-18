1fe4047c60c7ebe3af815c64091002aa5b501f35
Sagnik Haldar <sagnikh2@gmail.com>
2020-07-18 11:17:40 +0530

    Added the inline submit button

--- records/urls.py
+++ records/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'records'
 
 urlpatterns = [
-    path('', views.RecordListView.as_view(), name='todoList'),
+    # path('', views.RecordListView.as_view(), name='todoList'),
+    path('', views.recordListView, name='todoList'),
     path('delete/<int:pk>', views.RecordDeleteView.as_view(), name='delete'),
     path('create/', views.RecordCreateView.as_view(), name='create'),
 ]
--- records/views.py
+++ records/views.py
@@ -2,26 +2,39 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.urls import reverse
 from django.views.generic import DeleteView, ListView, CreateView
 
+from django import forms
+
 from .models import RecordList
 
 from .forms import RecordListForm
 
 # Create your views here.
 
-# def recordListView(request) :
+def recordListView(request) :
 
-#     items = RecordList.objects.all()
+    items = RecordList.objects.all()
 
-#     return render(request, 'index.html', {'items' : items})
+    if request.method == 'POST' :
+        val = request.POST['item']
+
+        if len(val) == 0 :
+            error = 'Not a valid Input'
+            return render(request, 'index.html', {'items' : items, 'errors' : error})
+
+        RecordList.objects.create(todo=val)
+
+        return redirect('/')
+
+    return render(request, 'index.html', {'items' : items})
 
 
 
 # class based ListView
-class RecordListView(ListView) :
+# class RecordListView(ListView) :
 
-    template_name = "index.html"
+#     template_name = "index.html"
 
-    queryset = RecordList.objects.all()
+#     queryset = RecordList.objects.all()
 
 
 
